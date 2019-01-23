21d2ae18547b9b7dbc33aca4c44d407b244a38f2
Tyrell <Tyrell102@gmail.com>
2019-01-23 12:27:35 -0500

    saving

--- src/StateDataProject.java
+++ src/StateDataProject.java
@@ -1,10 +1,16 @@
-/* Author: Tyrell Robbins
+/*
+Author: Tyrell Robbins
+Course CSCI 112
+This project will be made up of 3 class files, one for the main method,
+the second for reading data from a .txt file into an array and the third
+will be for the information for each state
 
 * */
 
 
-public class StateDataProject {
+public class StateDataProject throws Exception {
 
+    // The main method will act as the starting point for this program
     public static void main(String[] args){
 
 
--- src/StateList.java
+++ src/StateList.java
@@ -1,2 +1,18 @@
-public class StateList {
-}
+import java.util.Scanner;
+import java.io.File;
+
+
+// This class will be responsible for loading the data from the .txt file into an array
+public class StateList{
+
+    int count = 0;
+    String line;
+    String fileName = ("statedata.txt");
+
+    Scanner infile = new Scanner(new File(fileName));
+
+
+
+
+
+}// end StateList
